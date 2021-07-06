8fad2b2c2c420d272d66b248e996e96ca32f547d
Hariprasath R <balaprasad24@gmail.com>
2021-07-06 07:33:49 +0530

    Initial Commit

---
+++ src/main/java/in/hp/java/libraryservice/LibraryServiceApplication.java
@@ -0,0 +1,13 @@
+package in.hp.java.libraryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/hp/java/libraryservice/LibraryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package in.hp.java.libraryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
